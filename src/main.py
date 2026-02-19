dce3750dfc509847e169ef2fa3a6f38f9d6497ec
William C <will.chamberlayne@gmail.com>
2026-02-19 04:39:53 +0000

    add static files and styles; implement copy_static_files function and update main to use it

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from textnode import TextNode, TextType
+from copy_static import copy_static_files
 
 def main():
-    node = TextNode('hello world', text_type=TextType.PLAIN, url = 'https://www.warpedvulpes.ca')
+    node = TextNode('hello world', text_type=TextType.TEXT, url = 'https://www.warpedvulpes.ca')
     print(node)
+    copy_static_files('./static', './public')
+
 
 main()
