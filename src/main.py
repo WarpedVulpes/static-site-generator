1aee3ff1bfcc70c80d2357b847dce22b05ef5107
William C <will.chamberlayne@gmail.com>
2026-02-20 17:44:20 +0000

    Add initial site structure with blog posts, contact page, and styling
    - Create build script to run the site generator
    - Add .nojekyll file to prevent Jekyll processing
    - Implement blog posts: "Why Glorfindel is More Impressive than Legolas", "The Unparalleled Majesty of 'The Lord of the Rings'", and "Why Tom Bombadil Was a Mistake"
    - Create contact page for author communication
    - Add images for blog posts and author
    - Develop CSS for site styling
    - Update main.py and web_utils.py to support dynamic base paths for links and resources

--- src/main.py
+++ src/main.py
@@ -1,11 +1,18 @@
+import sys
+
 from textnode import TextNode, TextType
 from web_utils import copy_static_files, generate_pages_recursive
 
 def main():
+    if len(sys.argv) < 2:
+        basepath = '/'
+    else:
+        basepath = sys.argv[1]
+    
     node = TextNode('hello world', text_type=TextType.TEXT, url = 'https://www.warpedvulpes.ca')
     print(node)
-    copy_static_files('./static', './public')
-    generate_pages_recursive('./content', 'template.html', './public')
+    copy_static_files('./static', './docs')
+    generate_pages_recursive('./content', 'template.html', './docs', basepath)
     
 
 
--- src/web_utils.py
+++ src/web_utils.py
@@ -26,7 +26,7 @@ def extract_title(markdown):
             return line[2:].strip()
     raise Exception('No title found')
         
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using template {template_path}")
     with open(from_path, 'r') as f:
         content = f.read()
@@ -37,20 +37,22 @@ def generate_page(from_path, template_path, dest_path):
     html_content = html_content.to_html()
     final_content = re.sub(r"\{\{\s*title\s*\}\}", title, temp_content, flags=re.IGNORECASE)
     final_content = re.sub(r"\{\{\s*content\s*\}\}", html_content, final_content, flags=re.IGNORECASE)
+    final_content = final_content.replace('href="/', f'href="{basepath}')
+    final_content = final_content.replace('src="/', f'src="{basepath}')
     with open(dest_path, 'w') as out:
         out.write(final_content)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for item in os.listdir(dir_path_content):
         content_item = os.path.join(dir_path_content, item)
         dest_item = os.path.join(dest_dir_path, item)
 
         if os.path.isfile(content_item) and content_item.endswith('.md'):
             dest_item = dest_item[:-3] + '.html'
-            generate_page(content_item, template_path, dest_item)
+            generate_page(content_item, template_path, dest_item, basepath)
         elif os.path.isdir(content_item):
             if not os.path.exists(dest_item):
                 os.mkdir(dest_item)
-            generate_pages_recursive(content_item, template_path, dest_item)
+            generate_pages_recursive(content_item, template_path, dest_item, basepath)
         
